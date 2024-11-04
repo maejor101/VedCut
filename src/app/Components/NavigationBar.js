1e1126aa79ba63f3807e39910310f4b7b886aac7
Mochene <tshegofatsopromise362@gmail.com>
2024-11-04 12:18:44 +0200

    04/11/2024

--- src/app/Components/NavigationBar.js
+++ src/app/Components/NavigationBar.js
@@ -5,9 +5,10 @@ import HomeIcon from '@mui/icons-material/Home';
 import InfoIcon from '@mui/icons-material/Info';
 import BuildIcon from '@mui/icons-material/Build';
 import ContactMailIcon from '@mui/icons-material/ContactMail';
-import logo2 from '../Screens/assets/logo2.png'; 
+import logo2 from '../Screens/assets/logo2.png';
 import Image from 'next/image';
 import { Alarm, Notifications, Person, Settings } from '@mui/icons-material';
+import Link from 'next/link';
 
 function HamburgerMenu() {
   const [drawerOpen, setDrawerOpen] = useState(false);
@@ -24,11 +25,11 @@ function HamburgerMenu() {
   return (
     <>
       {/* AppBar with Hamburger Icon on the Right */}
-      <AppBar position="static" style={{backgroundColor:"#fafafa"}}>
+      <AppBar position="static" style={{ backgroundColor: "#fafafa" }}>
         <Toolbar>
           <Box sx={{ mr: 2 }}>
             <Image
-              src={logo2} 
+              src={logo2}
               alt="App Logo"
               width={50}
               height={50}
@@ -36,7 +37,7 @@ function HamburgerMenu() {
           </Box>
           <Box sx={{ flexGrow: 1 }} />
           <IconButton
-            edge="end" 
+            edge="end"
             sx={{ color: '#4B5563' }}
             aria-label="menu"
             onClick={toggleDrawer(true)}
@@ -46,34 +47,34 @@ function HamburgerMenu() {
         </Toolbar>
       </AppBar>
 
-      <Drawer anchor="right"  open={drawerOpen} onClose={toggleDrawer(false)}>
-        <List sx={{height:"100%"}}>
-          <ListItem button>
-            <ListItemIcon>
-              <Person sx={{color:'#3B82F6'}}/>
-            </ListItemIcon>
-            <ListItemText primary="Profile" sx={{color:'#333333'}}/>
-          </ListItem>
-          <ListItem button>
-            <ListItemIcon>
-              <Notifications sx={{color:'#3B82F6'}}/>
-            </ListItemIcon>
-            <ListItemText primary="Subscribe" sx={{color:'#333333'}}/>
-          </ListItem>
-          <ListItem button>
-            <ListItemIcon>
-              <Settings sx={{color:'#3B82F6'}}/>
-            </ListItemIcon>
-            <ListItemText primary="Settings" sx={{color:'#333333'}}/>
-          </ListItem>
+      <Drawer anchor="right" open={drawerOpen} onClose={toggleDrawer(false)}>
+        <List sx={{ height: "100%" }}>
+          <Link href="/Screens/profile">
+            <ListItem button>
+              <ListItemIcon>
+                <Person sx={{ color: '#3B82F6' }} />
+              </ListItemIcon>
+              <ListItemText primary="Profile" sx={{ color: '#333333' }} />
+            </ListItem>
+          </Link>
+          <Link href="/Screens/subscription">
+            <ListItem button>
+              <ListItemIcon>
+                <Notifications sx={{ color: '#3B82F6' }} />
+              </ListItemIcon>
+              <ListItemText primary="Subscribe" sx={{ color: '#333333' }} />
+            </ListItem>
+          </Link>
+          <Link href="/Screens/settings">
+            <ListItem button>
+              <ListItemIcon>
+                <Settings sx={{ color: '#3B82F6' }} />
+              </ListItemIcon>
+              <ListItemText primary="Settings" sx={{ color: '#333333' }} />
+            </ListItem>
+          </Link>
+
         </List>
-        {/* <Divider /> */}
-        {/* Sign Out Button */}
-        {/* <List sx={{backgroundColor:'#3B82F6'}}>
-          <ListItem button onClick={handleSignOut} >
-            <ListItemText primary="Sign Out" sx={{color:'#E63946', textAlign: 'center'}} />
-          </ListItem>
-        </List> */}
         <button className='nav-button'>Sign Out</button>
       </Drawer>
     </>
